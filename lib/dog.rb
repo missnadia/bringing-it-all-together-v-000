4c29760d8c09c84c482210bc00a6412bb45c0b32
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-21 21:52:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,7 @@
 require_relative "../config/environment.rb"
 
 class Dog
-  attr_accessor :name, :breed
-  attr_reader :id
+  attr_accessor :name, :breed, :id
 
   def initialize(id = nil, name, breed)
     @id = id
