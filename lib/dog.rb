49d3d1a96e6635be340030b194c1d17243e98d14
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-21 22:01:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,6 +57,7 @@ class Dog
     SQL
 
     dog = DB[:conn].execute(sql)
+
     if !dog.empty?
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
@@ -64,6 +65,19 @@ class Dog
       dog = self.create(name: name, breed: breed)
     end
     dog
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.find_by_id(id)
